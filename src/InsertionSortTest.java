da67d57bcb6b1c9c90978ff73c138c7c1f702ad8
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 00:57:48 +0100

    Method added for generating an array of random data

--- src/InsertionSortTest.java
+++ src/InsertionSortTest.java
@@ -1,3 +1,25 @@
-public class InsertionSortTest {
+import java.util.Random;
 
+public class InsertionSortTest {
+    public static int[] generateRandomDataArray(
+            int size,
+            int bound
+    ) {
+        Random random = new Random();
+        int[] randomDataArray = new int[size];
+
+        for (int i = 0; i < randomDataArray.length; i++) {
+            randomDataArray[i] = random.nextInt(bound);
+        }
+
+        return randomDataArray;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+
+
+
+
