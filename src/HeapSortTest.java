757af14b74a2499db36a264911ba73a8f54a6820
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 00:04:40 +0100

    Random array created for size and bound

--- src/HeapSortTest.java
+++ src/HeapSortTest.java
@@ -1,3 +1,20 @@
-public class HeapSortTest {
+import java.util.Random;
 
+public class HeapSortTest {
+    public static int[] generateDataArray(
+            int size,
+            int bound
+    ) {
+        Random random = new Random();
+        int[] randomDataArray = new int[size];
+
+        for (int i = 0; i < randomDataArray.length; i++) {
+            randomDataArray[i] = random.nextInt(bound);
+        }
+
+        return randomDataArray;
+    }
+
+    public static void testAlgorithmTime() {
+    }
 }
