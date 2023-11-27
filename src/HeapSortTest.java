9020b35c386f88fefe451ae20660c3a2083f4bfe
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 00:13:06 +0100

    4 Parameters added to "testAlgorithmTime" method and

--- src/HeapSortTest.java
+++ src/HeapSortTest.java
@@ -21,5 +21,11 @@ public class HeapSortTest {
             int size,
             int bound
     ) {
+        long startTime = System.nanoTime();
+        HeapSort.sort(generateDataArray(size, bound));
+        long endTime = System.nanoTime();
+        double sec = ((double) (endTime - startTime) / 1000000000.0);
+
+        System.out.println(testNumber + ". Test : Time taken by " + algorithmName + ": " + sec + " sec, with " + size + " size data.");
     }
 }
