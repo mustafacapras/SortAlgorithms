ec44947dcd4a7e2b2b7ce330568d97e38519d591
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 11:09:12 +0100

    Method added for generating an array of random data

--- src/RadixSortTest.java
+++ src/RadixSortTest.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class RadixSortTest {
+
+    public static int[] generateDataArray(
+            final int size,
+            final int bound
+    ) {
+        Random random = new Random();
+        int[] randomDataArray = new int[size];
+
+        for (int i = 0; i < randomDataArray.length; i++) {
+            randomDataArray[i] = random.nextInt(bound);
+        }
+
+        return randomDataArray;
+    }
+
 
     public static void main(String[] args) {
 
