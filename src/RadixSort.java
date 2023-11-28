ca33e19ff3e22627087f78ab5c7415c509b958f9
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 01:53:33 +0100

    Implement RadixSort sort method

--- src/RadixSort.java
+++ src/RadixSort.java
@@ -1,3 +1,14 @@
+import java.util.Arrays;
+
 public class RadixSort {
+    public static int[] sort(int[] arr) {
+        int max = Arrays.stream(arr).max().orElse(0);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+
+        return arr;
+    }
 
 }
