d70c5ba322c62d070df8dd441fc72cf29642c3c8
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 01:32:10 +0100

    Added QuickSort sort method

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,6 +1,12 @@
 public class QuickSort {
 
-    public static void sort(int[] arr) {
+    public static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(arr, low, high);
 
+            /* Recursively sort elements before and after partition*/
+            sort(arr, low, partitionIndex - 1);
+            sort(arr, partitionIndex + 1, high);
+        }
     }
 }
