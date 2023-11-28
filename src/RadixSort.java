8b0096ebb875701d7a2715752507d8379fcd46ad
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 01:58:42 +0100

    Refactor RadixSort countingSort method for better readability

--- src/RadixSort.java
+++ src/RadixSort.java
@@ -22,5 +22,16 @@ public class RadixSort {
             count[(value / exp) % 10]++;
         }
 
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
     }
+
+
 }
