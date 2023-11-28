54adb0e09e4fb6a828cea5d9fb80f096d1664829
Mustafa Capras <mustafaacapras@gmail.com>
2023-11-28 01:01:09 +0100

    method added with 4 parameters and this tests the execution time of the Insertion Sort algorithm

--- src/InsertionSortTest.java
+++ src/InsertionSortTest.java
@@ -15,6 +15,20 @@ public class InsertionSortTest {
         return randomDataArray;
     }
 
+    public static void testAlgorithmTime(
+            String testNumber,
+            String algorithmName,
+            int size, int bound
+    ) {
+        long startTime = System.nanoTime();
+        InsertionSort.sort(generateRandomDataArray(size, bound));
+        long endTime = System.nanoTime();
+        double sec = ((double) (endTime - startTime) / 1000000000.0);
+
+        System.out.println(testNumber + ". Test : Time " + algorithmName + ": " + sec + " sec, with " + size + " size data.");
+    }
+
+
     public static void main(String[] args) {
 
     }
